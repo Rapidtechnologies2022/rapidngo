0e15dfb3a08daccc640f47b169bc7ec488b5a0dd
srltech <srltech390@gmail.com>
2022-12-05 17:17:26 +0530

    pages

--- src/Components/slide/Slide.js
+++ src/Components/slide/Slide.js
@@ -12,10 +12,10 @@ function Slide() {
                         <div className="row align-items-center">
                             <div className="col-md-6">
                                 <div className="banner-content">
-                                    <h6 data-wow-duration="1s" data-wow-delay="0.5s" className="wow fadeInUp sub-title text-primary">We Create Ideas</h6>
-                                    <h1 data-wow-duration="1.2s" data-wow-delay="1s" className="wow fadeInUp "> We Provide <span className="text-primary">Website</span> Solution For You</h1>
-                                    <p data-wow-duration="1.4s" data-wow-delay="1.5s" className="wow fadeInUp m-b30">We are end-to-end solution providers. We will take your idea and through an objective, structured consultation process, and deliver a full marketing, branding and web development solution.</p>
-                                    <a href="about-us-1" ><a data-wow-duration="1.6s" data-wow-delay="2s" className="wow fadeInUp btn btn-link d-inline-flex align-items-center" ><i className="fa fa-angle-right m-r10"></i>Learn More</a></a>
+                                  {/*   <h6 data-wow-duration="1s" data-wow-delay="0.5s" className="wow fadeInUp sub-title text-primary">We Create Ideas</h6> */}
+                                    <h1 data-wow-duration="1.2s" data-wow-delay="1s" className="wow fadeInUp ">  <span className="text-primary">Give a helping </span> hand to those who need it!</h1>
+                                    <p data-wow-duration="1.4s" data-wow-delay="1.5s" className="wow fadeInUp m-b30">When a child gets access to good food, it can change just about everything.</p>
+                                    <a href="about-us-1" ><a data-wow-duration="1.6s" data-wow-delay="2s" className="wow fadeInUp btn btn-link d-inline-flex align-items-center" ><i className="fa fa-angle-right m-r10"></i>Ongoing Programs</a></a>
                                 </div>
                             </div>
                             <div className="col-md-6">
